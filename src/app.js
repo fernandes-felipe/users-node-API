5c4609c483cc7d8d7f973847e739a8513d349898
Felipe <felipe.fernandes@catho.com>
2020-03-31 00:05:45 -0300

    users API

---
+++ src/app.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const router = express.Router();
+
+//load routes
+const indexRoute = require('./routes/index');
+const userRoute = require('./routes/users-route');   
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:false }));
+
+app.use('/', indexRoute);
+app.use('/users', userRoute);
+
+module.exports = app;
